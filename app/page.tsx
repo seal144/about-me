cbca4f4995560cb414f448e58e116286e0817238
RafalFak <rafalfak8@gmail.com>
2024-01-04 12:43:59 +0100

    feat: Navigation mock

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import BackgroundAnimation from '@/app/ui/home/background-animation';
 import HeroSection from '@/app/ui/home/hero-section';
+import Navigation from '@/app/ui/navigation';
 
 export default function Home() {
   return (
     <main className="flex flex-col">
       <BackgroundAnimation />
       <HeroSection />
+      <Navigation />
       <section className="min-h-screen bg-background">
         <h1 className="text-5xl">Example Section</h1>
         <p className="text-xl text-secondary">example </p>
