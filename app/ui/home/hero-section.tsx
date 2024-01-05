925de44511bc293944ef139a8ae5b1ff704fbd66
RafalFak <rafalfak8@gmail.com>
2024-01-05 08:44:29 +0100

    fix: Hero amends

--- app/ui/home/hero-section.tsx
+++ app/ui/home/hero-section.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image';
 export default function HeroSection() {
   return (
     <section className="min-h-screen flex flex-col justify-center items-center">
-      <Image src="/logo_wyc.png" height={100} width={140} className="mb-4 hidden sm:block" alt="logo" />
-      <Image src="/logo_wyc.png" height={70} width={98} className="mb-4 block sm:hidden" alt="logo" />
-      <h1 className="text-5xl mb-2 sm:text-6xl md:text-7xl">Rafał Fąk</h1>
-      <h2 className="text-2xl text-secondary sm:text-3xl md:text-4xl">Frontend Developer</h2>
+      <Image src="/logo_wyc.png" height={81} width={150} className="mb-4 hidden sm:block" alt="logo" />
+      <Image src="/logo_wyc.png" height={54} width={100} className="mb-4 block sm:hidden" alt="logo" />
+      <h1 className="text-center text-5xl mb-2 sm:text-6xl md:text-7xl">Rafał Fąk</h1>
+      <h2 className="text-center text-2xl text-secondary sm:text-3xl md:text-4xl">Frontend Developer</h2>
     </section>
   );
 }
