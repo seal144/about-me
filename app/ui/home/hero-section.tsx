283906a8edd2e6a7b96c63bc7f84d5d04dadb88f
RafalFak <rafalfak8@gmail.com>
2024-01-09 20:55:24 +0100

    style(hero-section): small amends

--- app/ui/home/hero-section.tsx
+++ app/ui/home/hero-section.tsx
@@ -3,12 +3,10 @@ import Image from 'next/image';
 export default function HeroSection() {
   return (
     <section className="min-h-screen flex flex-col justify-center items-center">
-      <Image src="/logo_wyc.png" height={162} width={300} className="mb-12 hidden sm:block" alt="logo" />
-      <Image src="/logo_wyc.png" height={108} width={200} className="mb-8 block sm:hidden" alt="logo" />
+      <Image src="/logo.png" height={165} width={300} className="mb-12 hidden sm:block" alt="logo" />
+      <Image src="/logo.png" height={110} width={200} className="mb-8 block sm:hidden" alt="logo" />
       <h1 className="text-center text-5xl mb-2 sm:text-6xl md:text-7xl">RAFAŁ FĄK</h1>
-      <h2 className="text-center drop-shadow-s text-2xl text-secondary sm:text-3xl md:text-4xl">
-        JavaScript Developer
-      </h2>
+      <h2 className="text-center drop-shadow-s text-xl text-secondary sm:text-3xl md:text-4xl">JavaScript Developer</h2>
     </section>
   );
 }
