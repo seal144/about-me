ea5180c413d50ea0f953fe10f89fc9ab6706b3ce
RafalFak <rafalfak8@gmail.com>
2024-01-10 17:34:12 +0100

    feat(fetchSkills): add sort by list_order

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -5,7 +5,7 @@ export const fetchSkills = async () => {
   try {
     const data = await sql<Skill>`SELECT * FROM aboutme_skills`;
 
-    return data.rows;
+    return data.rows.sort((a, b) => a.list_order - b.list_order);
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch skills data.');
