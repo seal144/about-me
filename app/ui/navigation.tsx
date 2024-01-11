595cac4f119dd7d32c643773d94711739e12586a
RafalFak <rafalfak8@gmail.com>
2024-01-11 10:51:55 +0100

    feat(navigation): add logo

--- app/ui/navigation.tsx
+++ app/ui/navigation.tsx
@@ -1,7 +1,14 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 const Navigation = ({ className }: { className?: string }) => {
   return (
-    <section className={`bg-background py-5 border-y sticky top-0 z-[100] ${className}`}>
-      <div className="container">Navigation</div>
+    <section className={`bg-background py-3 border-y sticky top-0 z-[100] ${className}`}>
+      <div className="container">
+        <Link href="/" className="block w-fit">
+          <Image src="/logo.png" height={36} width={66} alt="logo" />
+        </Link>
+      </div>
     </section>
   );
 };
