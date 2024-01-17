eefd1b18ef3b4c2046e8bea5365d12fc40d71a79
RafalFak <rafalfak8@gmail.com>
2024-01-17 09:09:02 +0100

    feat(experienceSection): use Chip for techs

--- app/ui/chip.tsx
+++ app/ui/chip.tsx
@@ -3,19 +3,27 @@ import { clsx } from 'clsx';
 const Chip = ({
   label,
   onPrimary = false,
+  small = false,
   skeleton = false,
   skeletonWidth = 'md',
 }: {
   label: string;
   onPrimary?: boolean;
+  small?: boolean;
   skeleton?: boolean;
   skeletonWidth?: 'sm' | 'md' | 'lg' | 'xlg';
 }) => {
   return (
     <div
-      className={clsx(`w-fit text-foreground rounded-full py-1 px-3 text-l drop-shadow-s sm:text-xl`, {
+      className={clsx(`whitespace-nowrap w-fit text-foreground rounded-full py-1 px-3 drop-shadow-s`, {
         'bg-primary': !onPrimary,
         'bg-background': onPrimary,
+        'py-[2px]': small,
+        'px-2': small,
+        'text-base': !small,
+        'text-sm': small,
+        'sm:text-xl': !small,
+        'sm:text-base': small,
         'animate-pulse': skeleton,
         'min-h-[36px]': skeleton,
         'min-w-[80px]': skeleton && skeletonWidth === 'sm',
