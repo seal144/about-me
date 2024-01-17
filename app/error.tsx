b5751382cc1bfe5e91b3138c5f7eda582a060e11
RafalFak <rafalfak8@gmail.com>
2024-01-17 10:19:17 +0100

    feat(root/error.ts): add reset button

--- app/error.tsx
+++ app/error.tsx
@@ -1,16 +1,18 @@
 'use client';
+import Button from '@/app/ui/button';
 
-export default function Error() {
+export default function Error({ reset }: { reset: () => void }) {
   return (
     <section className="container py-12">
       <div className="max-w-[800px] mx-auto text-center">
         <h2 className="text-xl font-bold mb-3 sm:text-2xl">Oh Snap! Something went wrong!</h2>
         <div className="text-md sm:text-lg">
-          <p>
+          <p className="mb-2">
             It appears that there is some problem with rendering the page due to some error. Sorry for the
             inconvenience. Please, try again later.
           </p>
-          <p>You can let me know, I would appreciate it.</p>
+          <p className="mb-4">You can let me know, I would appreciate it.</p>
+          <Button onClick={reset}>Reset</Button>
         </div>
       </div>
     </section>
