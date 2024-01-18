10d936aae9c6508c1382e288eeba84572dcda271
RafalFak <rafalfak8@gmail.com>
2024-01-19 00:10:23 +0100

    feat(ExperienceSection): SkeletonProjectCards

---
+++ app/ui/skeletonText.tsx
@@ -0,0 +1,68 @@
+import clsx from 'clsx';
+import { randomNumber } from '@/app/utils/math';
+
+const SkeletonText = ({
+  variant = 'p',
+  quantity = 1,
+  roundFull = false,
+}: {
+  variant?: 'p' | 'h3' | 'h4';
+  quantity?: number;
+  roundFull?: boolean;
+}) => {
+  const iterationArray = new Array(quantity).fill(0).map(() => {
+    const randomWidthVariant = randomNumber(4);
+    switch (randomWidthVariant) {
+      case 0:
+        return 'sm';
+      case 1:
+        return 'md';
+      case 2:
+        return 'lg';
+      case 3:
+        return 'xl';
+      default:
+        return 'md';
+    }
+  });
+  const pVariant = variant === 'p';
+  const h4Variant = variant === 'h4';
+  const h3Variant = variant === 'h3';
+
+  return (
+    <span className="opacity-40">
+      {iterationArray.map((width, index, array) => {
+        const lastElement = index === array.length - 1;
+        const smWidth = width === 'sm';
+        const mdWidth = width === 'md';
+        const lgWidth = width === 'lg';
+        const xlWidth = width === 'xl';
+        return (
+          <div
+            key={index}
+            className={clsx('animate-pulse inline-block', {
+              'rounded-lg': !roundFull,
+              'rounded-full': roundFull,
+              'w-[22px]': smWidth && pVariant,
+              'w-[42px]': mdWidth && pVariant,
+              'w-[62px]': lgWidth && pVariant,
+              'w-[82px]': xlWidth && pVariant,
+              'w-[60px]': smWidth && !pVariant,
+              'w-[80px]': mdWidth && !pVariant,
+              'w-[100px]': lgWidth && !pVariant,
+              'w-[120px]': xlWidth && !pVariant,
+              'h-[16px] bg-foreground': pVariant,
+              'h-[18px] bg-background': h4Variant,
+              'h-[24px] bg-background': h3Variant,
+              'mr-2': (pVariant || h4Variant) && !lastElement,
+              'mr-3': h3Variant && !lastElement,
+              'mr-0': lastElement,
+            })}
+          ></div>
+        );
+      })}
+    </span>
+  );
+};
+
+export default SkeletonText;
