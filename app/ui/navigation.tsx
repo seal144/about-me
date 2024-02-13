283ac78e16e53be6c2747bcb927849119ef6b70d
RafalFak <rafalfak8@gmail.com>
2024-02-13 11:11:23 +0100

    feat(navigation): logo click behavior

--- app/ui/navigation.tsx
+++ app/ui/navigation.tsx
@@ -1,13 +1,30 @@
+'use client';
+
+import { usePathname, useRouter } from 'next/navigation';
+
 import Image from 'next/image';
-import Link from 'next/link';
 
 const Navigation = ({ className }: { className?: string }) => {
+  const pathName = usePathname();
+  const router = useRouter();
+
+  const logoClick = () => {
+    if (pathName === '/') {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    } else {
+      router.push('/');
+    }
+  };
+
   return (
     <section className={`bg-background py-3 border-y sticky top-0 z-[100] ${className}`}>
       <div className="container">
-        <Link href="/" className="block w-fit">
+        <button onClick={logoClick} className="block">
           <Image src="/logo.png" height={36} width={66} alt="logo" />
-        </Link>
+        </button>
       </div>
     </section>
   );
