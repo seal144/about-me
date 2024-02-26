f029f3b961ea6021c0cd1d2ab4862042b64984b4
RafalFak <rafalfak8@gmail.com>
2024-02-26 23:00:24 +0100

    feat(layout): extract some components upwards

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import { robotoMono } from '@/app/ui/fonts';
+import Footer from '@/app/ui/footer';
+import BackgroundAnimation from '@/app/ui/home/background-animation';
 import '@/app/globals.css';
 
 export const metadata: Metadata = {
@@ -10,7 +12,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${robotoMono.className} antialiased`}>{children}</body>
+      <body className={`${robotoMono.className} antialiased`}>
+        <BackgroundAnimation />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
