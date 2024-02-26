fb90517b36c2b13217c73a190cfb0bf56163b0f9
RafalFak <rafalfak8@gmail.com>
2024-02-26 23:00:24 +0100

    feat(layout): extract some components upwards

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,14 +1,10 @@
 import Navigation from '@/app/ui/navigation';
-import Footer from '@/app/ui/footer';
-import BackgroundAnimation from '../ui/home/background-animation';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main>
-      <BackgroundAnimation />
+    <>
       <Navigation />
       {children}
-      <Footer />
-    </main>
+    </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
-import BackgroundAnimation from '@/app/ui/home/background-animation';
 import HeroSection from '@/app/ui/home/hero-section';
 import Navigation from '@/app/ui/navigation';
 import AboutMeSection from '@/app/ui/home/aboutMeSection';
 import SkillsSection from '@/app/ui/home/skillsSection';
 import ExperienceSection from '@/app/ui/home/experienceSection';
-import Footer from '@/app/ui/footer';
 
 export default function Home() {
   return (
-    <main>
-      <BackgroundAnimation />
+    <>
       <HeroSection />
       <Navigation />
       <AboutMeSection />
       <SkillsSection />
       <ExperienceSection />
-      <Footer />
-    </main>
+    </>
   );
 }
