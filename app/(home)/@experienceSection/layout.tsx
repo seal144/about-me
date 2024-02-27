226978e2dcd0c00d11a673378f55cedf53d23975
RafalFak <rafalfak8@gmail.com>
2024-02-27 01:23:58 +0100

    fix: ExperienceLayout

--- app/(home)/@experienceSection/layout.tsx
+++ app/(home)/@experienceSection/layout.tsx
@@ -3,13 +3,15 @@ import { HomeSections } from '@/app/lib/definitions';
 
 const ExperienceLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Section
-      title="Experience"
-      subtitle="Some of the commercial projects, that I have been working on:"
-      id={HomeSections.ExperienceSection}
-    >
-      <div className="flex flex-wrap justify-center gap-8 xl:gap-10 2xl:gap-12">{children}</div>
-    </Section>
+    <>
+      <Section
+        title="Experience"
+        subtitle="Some of the commercial projects, that I have been working on:"
+        id={HomeSections.ExperienceSection}
+      >
+        <div className="flex flex-wrap justify-center gap-8 xl:gap-10 2xl:gap-12">{children}</div>
+      </Section>
+    </>
   );
 };
 
