6a7418aab3499567955cbbebdc8f01f5161b5c6b
RafalFak <rafalfak8@gmail.com>
2024-02-27 23:24:17 +0100

    fix(@experienceSection): by resigning from layout.tsx. The layout is duplicated id page and error

--- app/(home)/@experienceSection/page.tsx
+++ app/(home)/@experienceSection/page.tsx
@@ -1,9 +1,8 @@
-import Section from '@/app/ui/section';
-import { HomeSections } from '@/app/lib/definitions';
-
 import { Suspense } from 'react';
 import ExperienceCards, { SkeletonExperienceCards } from '@/app/ui/experienceCards';
 import { fetchProjects } from '@/app/lib/data';
+import Section from '@/app/ui/section';
+import { HomeSections } from '@/app/lib/definitions';
 
 const FetchedExperienceCards = async () => {
   const data = await fetchProjects();
@@ -11,10 +10,19 @@ const FetchedExperienceCards = async () => {
 };
 
 const ExperienceSection = () => {
+  // the section and div are duplicated in error.tsx. These wrappers should be moved to layout.tsx (like in @skillsSection), but for some reason the layout file is omitted on deployment (it is absent on production)
   return (
-    <Suspense fallback={<SkeletonExperienceCards quantity={3} />}>
-      <FetchedExperienceCards />
-    </Suspense>
+    <Section
+      title="Experience"
+      subtitle="Some of the commercial projects, that I have been working on:"
+      id={HomeSections.ExperienceSection}
+    >
+      <div className="flex flex-wrap justify-center gap-8 xl:gap-10 2xl:gap-12">
+        <Suspense fallback={<SkeletonExperienceCards quantity={3} />}>
+          <FetchedExperienceCards />
+        </Suspense>
+      </div>
+    </Section>
   );
 };
 
